f46a61df9202621eb9408cc3b15139893fde977f
MarlonGun <marlonull30@gmail.com>
2026-03-12 19:43:56 -0500

    FEAT(app): add natiwind in project

---
+++ app/_layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import "./global.css"
+
+const RootLayout = () => {
+    return (
+        <View>
+            <Text>RootLayout</Text>
+        </View>
+    )
+}
+
+export default RootLayout
